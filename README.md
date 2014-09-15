285c065291a3c3fccb160d0578a8f63d71f932c1
muzk <nggomez@uc.cl>
2014-09-15 14:56:26 -0300

    locales y devise

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ BaseApplication
 ===============
 
 Creemos aquí la aplicación base :)
+
+
+## Devise ##
+
+```
+rails generate devise:install
+```
+
+Más información en <https://github.com/plataformatec/devise>
+
+## Cancancan ##
+
+```
+rails g cancan:ability
+```
+
+Más información en <https://github.com/CanCanCommunity/cancancan>
+
--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,14 @@ module BaseApplication
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.time_zone = 'Santiago'
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.i18n.default_locale = :es
+    I18n.enforce_available_locales = true
+
   end
 end
