529188c128207b2ce371ef1e3babc00f62833e57
muzk <nggomez@uc.cl>
2014-09-15 14:38:38 -0300

    basic gems

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+//= require select2
+//= require jquery_nested_form
+//= require jquery.datetimepicker
+//= require_self
+//= require_tree .
+
+function onInit(callback){
+    $(document).ready(callback);
+    $(document).on('page:load', callback);
+}
