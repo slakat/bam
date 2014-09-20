dfdede29a779a9cd45103bee1a35ca2571f0fce2
fjsandov <franciscosandoval15@gmail.com>
2014-09-20 15:23:21 -0300

    Base de archivos de seed por ambiente. Falta idear una forma de manejar scripts que se suben con el sistema en producción y que deben ser leidos por los seed también. Además debe permitir dividir la carga cuando el script del ambiente se extienda demasiado.  #1508189

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+['all', Rails.env].each do |seed|
+  seed_file = "#{Rails.root}/db/seeds/#{seed}.rb"
+  if File.exists?(seed_file)
+    puts "*** Loading #{seed} seed data"
+    require_relative seed_file
+  end
+end
+#TODO: Falta idear una forma de manejar scripts que se suben con el sistema en producción y que deben ser leidos por los seed también. Además debe permitir dividir la carga cuando el script del ambiente se extienda demasiado.
