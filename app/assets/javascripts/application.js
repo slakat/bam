90adeff8d5517c3bea9d207d777e01620932b89a
muzk <nggomez@uc.cl>
2014-11-23 16:29:37 -0300

    agregue parametros para datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,39 @@ function onInit(callback){
     $(document).ready(callback);
     $(document).on('page:load', callback);
 }
+
+/***********************************
+ * Datepicker
+************************************/
+
+var datetimepicker_params = {
+    lang: 'es',
+    format: 'd/m/Y H:i',
+    placeholder: 'dd/mm/aaaa hh:mm',
+    formatDate: 'd/m/Y',
+    formatTime: 'H:i',
+    allowTimes: ['08:30', '10:00', '11:30', '12:50', '14:00', '15:30', '17:00', '18:30', '20:00'],
+    dayOfWeekStart: 1,
+    scrollInput: false,
+    allowBlank: true
+};
+
+var datepicker_params = $.extend({}, datetimepicker_params, {datepicker: true,  timepicker: false, format: 'd/m/Y'});
+var timepicker_params = $.extend({}, datetimepicker_params, {datepicker: false, timepicker: true,  format: 'H:i'});
+
+function initContainer(container_id) {
+
+    var datepickers = $("[data-behaviour='datepicker']", container_id);
+    datepickers.datetimepicker(datepicker_params);
+
+    var timepickers = $("[data-behaviour='timepicker']", container_id);
+    timepickers.datetimepicker(timepicker_params);
+
+    var datetimepickers = $("[data-behaviour='datetimepicker']", container_id);
+    datetimepickers.datetimepicker(datetimepicker_params);
+
+}
+
+onInit(function(){
+    initContainer('#main-container');
+});
