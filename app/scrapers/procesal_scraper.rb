6b724c3a80f6932a71641acdaf55401184dfd3e4
Tomas Gunther <tpgunther@uc.cl>
2015-05-04 16:04:17 -0300

    scrapper name listo

--- app/scrapers/procesal_scraper.rb
+++ app/scrapers/procesal_scraper.rb
@@ -1,60 +1,80 @@
+#10696737-7
 class ProcesalScraper
   
   def self.search_by_rut(input)
     rut = input.split('-')
-  
-   a = Mechanize.new { |agent|
-  agent.user_agent_alias = 'Mac Safari'
-  }
+    a = Mechanize.new { |agent|
+      agent.user_agent_alias = 'Mac Safari'
+    }
 
- a.get('http://reformaprocesal.poderjudicial.cl/ConsultaCausasJsfWeb/page/panelConsultaCausas.jsf') do |page|
-  # search_result = page.form_with(:name => 'InicioAplicacionForm'){ |frm|
+    a.get('http://reformaprocesal.poderjudicial.cl/ConsultaCausasJsfWeb/page/panelConsultaCausas.jsf') do |page|
+      # search_result = page.form_with(:name => 'InicioAplicacionForm'){ |frm|
 
-  # }.submit
-  a.cookie_jar
-  #page = a.get("http://corte.poderjudicial.cl/SITCORTEPORWEB/AtPublicoViewAccion.do?tipoMenuATP=1")
-end
+      # }.submit
+      a.cookie_jar
+      #page = a.get("http://corte.poderjudicial.cl/SITCORTEPORWEB/AtPublicoViewAccion.do?tipoMenuATP=1")
+    end
 
-page = a.post('http://corte.poderjudicial.cl/SITCORTEPORWEB/AtPublicoDAction.do', {
-"TIP_Consulta"=>"2",
-"TIP_Lengueta"=>"tdRut",
-"TIP_Causa"=>"",
-"COD_Libro"=>"",
-"COD_Corte"=>"0",
-"ROL_Recurso"=>"",
-"ERA_Recurso"=>"",
-"FEC_Desde"=>"26/04/2015",
-"FEC_Hasta"=>"26/04/2015",
-"NOM_Consulta"=>"",
-"APE_Paterno"=>"",
-"APE_Materno"=>"",
-"selConsulta"=>"0",
-"ROL_Causa"=>"",
-"ERA_Causa"=>"",
-"RUC_Era"=>"",
-"RUC_Tribunal"=>"",
-"RUC_Numero"=>rut[0],
-"RUC_Dv"=>rut[1],
-"RUT_Consulta"=>rut[0],
-"RUT_DvConsulta"=>rut[1],
-"irAccionAtPublico"=>"Consulta"}, {'Cookie'=>"CRR_IdFuncionario=0; COD_TipoCargo=0; COD_Corte=90; COD_Usuario=autoconsulta; GLS_Corte=C.A. de Santiago; COD_Ambiente=3; COD_Aplicacion=3; GLS_Usuario=; HORA_LOGIN=03:49; NUM_SalaUsuario=0; #{a.cookies[0]};"})
+    @list=[]
+    tribunales = []
+    a.page.search('select[name="formConsultaCausas:idSelectedCodeTribunalNom"]').children.each do |n|
+      tribunales << n.attr("value")
+    end
+    tribunales.reject! { |c| c == "-1" || c.nil? }
 
+    ## probar distintos años. Ver si hay algun tribunal que sean todos
+    ## almacenar los resultados en la lista
+    tribunales.each do |tribunal|
+      page = a.post('http://reformaprocesal.poderjudicial.cl/ConsultaCausasJsfWeb/page/panelConsultaCausas.jsf', {
+      #{}"formConsultaCausas:idTabs"=>"idTabRut",
+      }"formConsultaCausas:idTabs"=>"idTabNombre",
+      "formConsultaCausas:idValueRadio"=>"1",      
+      #{}"formConsultaCausas:idFormRut"=>"10696737",
+      #{}"formConsultaCausas:idFormRutDv"=>"7",
+      "formConsultaCausas:idFormRolInterno"=>"",
+      "formConsultaCausas:idFormRolInternoEra"=>"",
+      "formConsultaCausas:idSelectedCodeTipCauRef"=>"",
+      "formConsultaCausas:idFormRolUnico"=>"",
+      "formConsultaCausas:idFormRolUnicoDv"=>"",
+      "formConsultaCausas:idSelectedCodeTribunal"=>"",
+      "formConsultaCausas:tblListaParticipantes:s"=>"-1",
+      "formConsultaCausas:tblListaRelaciones:s"=>"-1",
+      "formConsultaCausas:tblListaTramites:s"=>"-1",
+      "formConsultaCausas:tblListaNotificaciones:s"=>"-1",
+      "formConsultaCausas:idFormNombres"=>"",
+      "formConsultaCausas:idFormApPater"=>"",
+      "formConsultaCausas:idFormApMater"=>"",
+      "formConsultaCausas:idFormFecEra"=>"0",
+      "formConsultaCausas:idSelectedCodeTribunalNom"=>tribunal,
+      #{}"formConsultaCausas:idSelectedCodeTribunalNom"=>"1245",
+      "formConsultaCausas:buscar2.x"=>"50",
+      "formConsultaCausas:buscar2.y"=>"9",
+      "formConsultaCausas:tblListaConsultaNombres:s"=>"-1",
+      "formConsultaCausas:tblListaParticipantesNom:s"=>"-1",
+      "formConsultaCausas:tblListaRelacionesNom:"=>"-1",
+      "formConsultaCausas:tblListaTramitesNom:s"=>"-1",
+      "formConsultaCausas:tblListaNotificacionesNom:s"=>"-1",
+      "formConsultaCausas:waitCargaSentOpenedState"=>"",
+      "formConsultaCausas"=>"formConsultaCausas",
+      "autoScroll"=>"",
+      "javax.faces.ViewState"=>a.page.forms[0]['javax.faces.ViewState']})
 
-  @list=[]
-  #puts page.search("table#filaSel tr").inner_text
-  page.search('div#divRecursos table tr.textoPortal').each do |n|
-    properties = n.search('td a/text()','td/text()').collect {|text| text.to_s}
-    things = [properties[0].strip,properties[3],properties[4],properties[5],properties[6],properties[7]]
-    @list << (things)
-  end
-  puts @list.first
-  puts @list.first[0]
-  puts @list.first[1]
-  puts @list.first[2]
-  puts @list.first[3]
+      @list=[]
+      page.search("tr.extdt-firstrow.rich-extdt-firstrow").each do |n|
+        #puts 1
+        properties = []
+        n.search('.texto').each do |content|
+              properties << content.content
+        end
+        #puts properties            
+        things = [properties[0].strip,properties[1], "#{properties[2]}-#{properties[3]}", properties[4], properties[5]]
+        #puts things
+        @list << (things)
+      end
+    end
 
-  return @list
-
+        
+    return @list
   end
   
   def self.search_by_name(a,b,c)
@@ -99,7 +119,7 @@ page = a.post('http://corte.poderjudicial.cl/SITCORTEPORWEB/AtPublicoDAction.do'
       "formConsultaCausas:idFormNombres"=>name,
       "formConsultaCausas:idFormApPater"=>last_name,
       "formConsultaCausas:idFormApMater"=>second_last_name,
-      "formConsultaCausas:idFormFecEra"=>"2014",
+      "formConsultaCausas:idFormFecEra"=>"0",
       "formConsultaCausas:idSelectedCodeTribunalNom"=>tribunal,
       "formConsultaCausas:buscar2.x"=>"50",
       "formConsultaCausas:buscar2.y"=>"9",
@@ -113,32 +133,87 @@ page = a.post('http://corte.poderjudicial.cl/SITCORTEPORWEB/AtPublicoDAction.do'
       "autoScroll"=>"",
       "javax.faces.ViewState"=>a.page.forms[0]['javax.faces.ViewState']})
 
-      page.search(".texto").each do |n|
-        puts n.content
+      @list=[]
+      page.search("tr.extdt-firstrow.rich-extdt-firstrow").each do |n|
+        #puts 1
+        properties = []
+        n.search('.texto').each do |content|
+              properties << content.content
+        end
+        #puts properties            
+        things = [properties[0].strip,properties[1], "#{properties[2]}-#{properties[3]}", properties[4], properties[5]]
+        #puts things
+        @list << (things)
       end
     end
 
-    
-  
-    #puts page.search("table#filaSel tr").inner_text
-    
-    # page.search('.texto').each do |n|
-    #   properties = n.search('.td_GLS_RitC/text() a/text()','td/text()','.td_GLS_GeneralFecha/text()','td.td_GLS_GeneralMedium/text()','td.td_GLS_GeneralFecha/text()','td.td_GLS_GeneralMedium/text()','td.td_GLS_GeneralLong/text()').collect {|text| text.to_s}
-    #   puts properties
-    #   things = [properties[0].strip,properties[3],properties[4],properties[5],properties[6],properties[7]]
-    #   @list << (things)
-    # end
-
-    # puts @list.first[0]
-    # puts @list.first[1]
-    # puts @list.first[2]
-    # puts @list.first[3]
-    # puts @list.first[4]
-    # puts @list.first[5]
-
-    # return @list
-
-
+        
+    return @list
   end
 
 end
+
+
+
+
+a = Mechanize.new { |agent|
+      agent.user_agent_alias = 'Mac Safari'
+    }
+
+    a.get('http://reformaprocesal.poderjudicial.cl/ConsultaCausasJsfWeb/page/panelConsultaCausas.jsf') do |page|
+      # search_result = page.form_with(:name => 'InicioAplicacionForm'){ |frm|
+
+      # }.submit
+      a.cookie_jar
+      #page = a.get("http://corte.poderjudicial.cl/SITCORTEPORWEB/AtPublicoViewAccion.do?tipoMenuATP=1")
+    end
+
+# Probando cosas......
+# page = a.post('http://reformaprocesal.poderjudicial.cl/ConsultaCausasJsfWeb/page/panelConsultaCausas.jsf', {
+#       "formConsultaCausas:idTabs"=>"idTabRut",
+#       "formConsultaCausas:idValueRadio"=>"1",      
+#       "formConsultaCausas:idFormRut"=>"10696737",
+#       "formConsultaCausas:idFormRutDv"=>"7",
+#       "formConsultaCausas:idFormRolInterno"=>"",
+#       "formConsultaCausas:idFormRolInternoEra"=>"",
+#       "formConsultaCausas:idSelectedCodeTipCauRef"=>"",
+#       "formConsultaCausas:idFormRolUnico"=>"",
+#       "formConsultaCausas:idFormRolUnicoDv"=>"",
+#       "formConsultaCausas:idSelectedCodeTribunal"=>"",
+#       "formConsultaCausas:tblListaParticipantes:s"=>"-1",
+#       "formConsultaCausas:tblListaRelaciones:s"=>"-1",
+#       "formConsultaCausas:tblListaTramites:s"=>"-1",
+#       "formConsultaCausas:tblListaNotificaciones:s"=>"-1",
+#       "formConsultaCausas:idFormNombres"=>"",
+#       "formConsultaCausas:idFormApPater"=>"",
+#       "formConsultaCausas:idFormApMater"=>"",
+#       "formConsultaCausas:idFormFecEra"=>"0",
+#       "formConsultaCausas:idSelectedCodeTribunalNom"=>"1245",
+#       "formConsultaCausas:buscar2.x"=>"50",
+#       "formConsultaCausas:buscar2.y"=>"9",
+#       "formConsultaCausas:tblListaConsultaNombres:s"=>"-1",
+#       "formConsultaCausas:tblListaParticipantesNom:s"=>"-1",
+#       "formConsultaCausas:tblListaRelacionesNom:"=>"-1",
+#       "formConsultaCausas:tblListaTramitesNom:s"=>"-1",
+#       "formConsultaCausas:tblListaNotificacionesNom:s"=>"-1",
+#       "formConsultaCausas:waitCargaSentOpenedState"=>"",
+#       "formConsultaCausas"=>"formConsultaCausas",
+#       "autoScroll"=>"",
+#       "javax.faces.ViewState"=>a.page.forms[0]['javax.faces.ViewState']})
+
+#       # @list=[]
+#       # page.search("tr.extdt-firstrow.rich-extdt-firstrow").each do |n|
+#       #   #puts 1
+#       #   properties = []
+#       #   n.search('.texto').each do |content|
+#       #         properties << content.content
+#       #   end
+#       #   #puts properties            
+#       #   things = [properties[0].strip,properties[1], "#{properties[2]}-#{properties[3]}", properties[4], properties[5]]
+#       #   #puts things
+#       #   @list << (things)
+#       # end
+
+#       page.search(".texto").each do |n|
+#         puts n.content
+#       end
