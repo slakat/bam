62369d22c41142f9dcb8682f798cabc34bb4c096
Tomas Gunther <tpgunther@uc.cl>
2015-05-06 15:20:57 -0300

    rut todavia no funciona

--- app/scrapers/procesal_scraper.rb
+++ app/scrapers/procesal_scraper.rb
@@ -26,11 +26,11 @@ class ProcesalScraper
     ## almacenar los resultados en la lista
     tribunales.each do |tribunal|
       page = a.post('http://reformaprocesal.poderjudicial.cl/ConsultaCausasJsfWeb/page/panelConsultaCausas.jsf', {
-      #{}"formConsultaCausas:idTabs"=>"idTabRut",
-      }"formConsultaCausas:idTabs"=>"idTabNombre",
+      #"formConsultaCausas:idTabs"=>"idTabRut",
+      "formConsultaCausas:idTabs"=>"idTabNombre",
       "formConsultaCausas:idValueRadio"=>"1",      
-      #{}"formConsultaCausas:idFormRut"=>"10696737",
-      #{}"formConsultaCausas:idFormRutDv"=>"7",
+      "formConsultaCausas:idFormRut"=>"10696737",
+      "formConsultaCausas:idFormRutDv"=>"7",
       "formConsultaCausas:idFormRolInterno"=>"",
       "formConsultaCausas:idFormRolInternoEra"=>"",
       "formConsultaCausas:idSelectedCodeTipCauRef"=>"",
@@ -45,8 +45,7 @@ class ProcesalScraper
       "formConsultaCausas:idFormApPater"=>"",
       "formConsultaCausas:idFormApMater"=>"",
       "formConsultaCausas:idFormFecEra"=>"0",
-      "formConsultaCausas:idSelectedCodeTribunalNom"=>tribunal,
-      #{}"formConsultaCausas:idSelectedCodeTribunalNom"=>"1245",
+      "formConsultaCausas:idSelectedCodeTribunalRut"=>tribunal,      
       "formConsultaCausas:buscar2.x"=>"50",
       "formConsultaCausas:buscar2.y"=>"9",
       "formConsultaCausas:tblListaConsultaNombres:s"=>"-1",
@@ -153,67 +152,3 @@ class ProcesalScraper
 
 end
 
-
-
-
-a = Mechanize.new { |agent|
-      agent.user_agent_alias = 'Mac Safari'
-    }
-
-    a.get('http://reformaprocesal.poderjudicial.cl/ConsultaCausasJsfWeb/page/panelConsultaCausas.jsf') do |page|
-      # search_result = page.form_with(:name => 'InicioAplicacionForm'){ |frm|
-
-      # }.submit
-      a.cookie_jar
-      #page = a.get("http://corte.poderjudicial.cl/SITCORTEPORWEB/AtPublicoViewAccion.do?tipoMenuATP=1")
-    end
-
-# Probando cosas......
-# page = a.post('http://reformaprocesal.poderjudicial.cl/ConsultaCausasJsfWeb/page/panelConsultaCausas.jsf', {
-#       "formConsultaCausas:idTabs"=>"idTabRut",
-#       "formConsultaCausas:idValueRadio"=>"1",      
-#       "formConsultaCausas:idFormRut"=>"10696737",
-#       "formConsultaCausas:idFormRutDv"=>"7",
-#       "formConsultaCausas:idFormRolInterno"=>"",
-#       "formConsultaCausas:idFormRolInternoEra"=>"",
-#       "formConsultaCausas:idSelectedCodeTipCauRef"=>"",
-#       "formConsultaCausas:idFormRolUnico"=>"",
-#       "formConsultaCausas:idFormRolUnicoDv"=>"",
-#       "formConsultaCausas:idSelectedCodeTribunal"=>"",
-#       "formConsultaCausas:tblListaParticipantes:s"=>"-1",
-#       "formConsultaCausas:tblListaRelaciones:s"=>"-1",
-#       "formConsultaCausas:tblListaTramites:s"=>"-1",
-#       "formConsultaCausas:tblListaNotificaciones:s"=>"-1",
-#       "formConsultaCausas:idFormNombres"=>"",
-#       "formConsultaCausas:idFormApPater"=>"",
-#       "formConsultaCausas:idFormApMater"=>"",
-#       "formConsultaCausas:idFormFecEra"=>"0",
-#       "formConsultaCausas:idSelectedCodeTribunalNom"=>"1245",
-#       "formConsultaCausas:buscar2.x"=>"50",
-#       "formConsultaCausas:buscar2.y"=>"9",
-#       "formConsultaCausas:tblListaConsultaNombres:s"=>"-1",
-#       "formConsultaCausas:tblListaParticipantesNom:s"=>"-1",
-#       "formConsultaCausas:tblListaRelacionesNom:"=>"-1",
-#       "formConsultaCausas:tblListaTramitesNom:s"=>"-1",
-#       "formConsultaCausas:tblListaNotificacionesNom:s"=>"-1",
-#       "formConsultaCausas:waitCargaSentOpenedState"=>"",
-#       "formConsultaCausas"=>"formConsultaCausas",
-#       "autoScroll"=>"",
-#       "javax.faces.ViewState"=>a.page.forms[0]['javax.faces.ViewState']})
-
-#       # @list=[]
-#       # page.search("tr.extdt-firstrow.rich-extdt-firstrow").each do |n|
-#       #   #puts 1
-#       #   properties = []
-#       #   n.search('.texto').each do |content|
-#       #         properties << content.content
-#       #   end
-#       #   #puts properties            
-#       #   things = [properties[0].strip,properties[1], "#{properties[2]}-#{properties[3]}", properties[4], properties[5]]
-#       #   #puts things
-#       #   @list << (things)
-#       # end
-
-#       page.search(".texto").each do |n|
-#         puts n.content
-#       end
