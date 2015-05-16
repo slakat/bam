28b53517b6b168e589f21aee0288b2623b725230
slakat <slakat@gmail.com>
2015-05-16 19:35:44 -0400

    testing deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -19,7 +19,7 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '104.236.234.135', user: 'deploy', roles: %w{web app}
+server '104.236.234.135', user: 'root', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
