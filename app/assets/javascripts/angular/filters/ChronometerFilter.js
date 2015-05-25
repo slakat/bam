b08c04799267e07933513c3c4f7873dcfccbe613
Katherine <slakat@gmail.com>
2015-05-25 17:34:04 -0300

    fixing error de nombre en controller

--- app/assets/javascripts/angular/filters/ChronometerFilter.js
+++
@@ -1,9 +0,0 @@
-app.filter('chronometer', function() {
-
-    return function(seconds) {
-        var date = new Date(null);
-        date.setSeconds(seconds);
-        return date.toISOString().substr(11, 8);
-    };
-
-});
