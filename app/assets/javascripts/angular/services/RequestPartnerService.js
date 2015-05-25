29d26be9e60b0a29e23f14d096ae264c6688e1ba
Katherine <slakat@gmail.com>
2015-05-25 17:27:10 -0300

    fixing error de nombre en controller

--- app/assets/javascripts/angular/services/RequestPartnerService.js
+++
@@ -1,9 +0,0 @@
-app.factory("RequestPartnerService", ['$resource',function($resource) {
-    return $resource("/request_partner_services/:id", { id: "@id" },
-        {
-            'index':   { method: 'GET', isArray: true },
-            'show':    { method: 'GET', isArray: false },
-            'update':  { method: 'PUT' }
-        }
-    );
-}]);
